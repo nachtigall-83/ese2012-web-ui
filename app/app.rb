97c776afeec643ee6e732936a8580d53933ac304
nachtigall-83 <nachtigall_83@yahoo.de>
2012-10-02 07:46:51 +0200

    app.rb file updated

--- app/app.rb
+++ app/app.rb
@@ -1,3 +1,24 @@
-class App
-  # To change this template use File | Settings | File Templates.
+require 'rubygems'
+require 'sinatra'
+require 'tilt/haml'
+require_relative 'models/trading/user'
+require '../app/controllers/main'
+require '../app/controllers/authentication'
+
+class App < Sinatra::Base
+
+  use Authentication
+  use Main
+
+  enable :sessions
+  set :public_folder, 'app/public'
+
+  configure :development do
+    University::User.named( 'Erwann' ).save()
+    University::User.named( 'Joel' ).save()
+    University::User.named( 'Aaron').save()
+    University::Item.named( 'cookie', 5, Erwann).save()
 end
+
+# Now, run it
+App.run!
